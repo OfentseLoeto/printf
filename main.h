f29379e34016bc2a2e0208a1fff2a6bfbe353c2e
OhentseLoeto <ohentseloeto610@gmail.com>
2022-11-28 10:53:17 -0800

    Created task 0

---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+typedef struct specifiers
+{
+
+	char *specifier;
+
+	int (*f)(va_list args);
+
+} spc_dt;
+
+int _printf(const char *format, ...);
+void _putchar(char c);
+int _print_a_char(va_list args);
+int _print_a_string(va_list args);
+int _print_format(const char *format, va_list args);
+
+#endif /*MAIN_H*/
---
+++ main2.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+void putchar(char c);
+
+#endif
+
